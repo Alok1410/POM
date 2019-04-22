0a2473f5128118180530d6c3f5e583b18543f914
Alok Sharma <Alok Sharma@QA_Alok-PC-83>
2019-04-22 20:08:29 +0530

    Final

---
+++ 2.3/src/test/aLOK.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class aLOK
+{
+
+	@Test
+	public void test()
+	{
+		String url = "http://api.ytzi.co/t/clk?id=7YUOCBQT82s2=41414141";
+		System.out.println(url.substring(url.lastIndexOf("=") - 2));
+	}
+}
