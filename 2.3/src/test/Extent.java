a97314b5cd9d00d5ed7f71853d32aabd2f38c01a
Alok <Alok@LAPTOP-1JHII323>
2019-04-25 00:49:32 +0530

    MavenExtentreport3.14

--- 2.3/src/test/Extent.java
+++ 2.3/src/test/Extent.java
@@ -7,76 +7,83 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class Extent
-{
+    {
+        
+        // builds a new report using the html template
+        static ExtentHtmlReporter htmlReporter;
 
-	static ExtentReports report;
-	//helps to generate the logs in test report.
-	static ExtentTest Logger;
+        static ExtentReports      extent;
+        // helps to generate the logs in test report.
+        static ExtentTest         test;
 
-	@BeforeClass
+        @BeforeClass
 
-	public static void startTest()
+        public static void startTest()
 
-	{
-		report = new ExtentReports(System.getProperty("user.dir") + "\\ExtentReportResults.html", true);
+            {// initialize the HtmlReporter
+                htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\ExtentReportResults.html");
 
-	}
+                // initialize ExtentReports and attach the HtmlReporter
+                extent = new ExtentReports();
+                extent.attachReporter(htmlReporter);
 
-	@Test
-	public void test123()
-	{
-		Logger = report.startTest("test123");
-		Assert.assertTrue(false);
+            }
 
-	}
+        @Test
+        public void test123()
+            {
+                test = extent.createTest("test123");
+                Assert.assertTrue(false);
 
-	@Test
-	public void test1234()
-	{
-		Logger = report.startTest("test1234");
-		System.out.println("testing");
+            }
 
-	}
+        @Test
+        public void test1234()
+            {
+                test = extent.createTest("test1234");
+                System.out.println("testing");
 
-	@Test
-	public void call()
-	{
-		Logger = report.startTest("call");
-		aLOK a = new aLOK();
-		a.test();
-		a.test1();
+            }
 
-	}
+        @Test
+        public void call()
+            {
+                test = extent.createTest("call");
+                aLOK a = new aLOK();
+                a.test();
+                a.test1();
 
-	@AfterMethod
-	public void getResult(ITestResult result)
-	{
-		if (result.getStatus() == ITestResult.FAILURE)
-		{
-			Logger.log(LogStatus.FAIL, "Test Case Failed is " + result.getName());
-			Logger.log(LogStatus.FAIL, "Test Case Failed is " + result.getThrowable());
-		}
-		else if (result.getStatus() == ITestResult.SUCCESS)
-		{
-			Logger.log(LogStatus.PASS, "Test Case Passed is " + result.getName());
-		}
+            }
 
-		//report.endTest(Logger);
-	}
+        @AfterMethod
+        public void getResult(ITestResult result)
+            {
+                if (result.getStatus() == ITestResult.FAILURE)
+                    {
+                        test.log(Status.FAIL, "Test Case Failed is " + result.getName());
+                        test.fail(result.getThrowable());
+                    } else if (result.getStatus() == ITestResult.SUCCESS)
+                    {
+                        test.log(Status.PASS, "Test Case Passed is " + result.getName());
+                    }
 
-	@AfterClass
+                // report.endTest(Logger);
+            }
 
-	public static void endTest()
+        @AfterClass
 
-	{
+        public static void endTest()
 
-		report.flush();
+            {
 
-	}
+                extent.flush();
 
-}
+            }
+
+    }
--- 2.3/src/test/aLOK.java
+++ 2.3/src/test/aLOK.java
@@ -4,18 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class aLOK
-{
-
-	@Test
-	public void test()
-	{
-		String url = "http://api.ytzi.co/t/clk?id=7YUOCBQT82s2=41414141";
-		System.out.println(url.substring(url.lastIndexOf("=") - 2));
-	}
-
-	@Test
-	public void test1()
-	{
-		Assert.assertEquals("an", "a");
-	}
-}
+    {
+        
+        @Test
+        public void test()
+            {
+                String url = "http://api.ytzi.co/t/clk?id=7YUOCBQT82s2=41414141";
+                System.out.println(url.substring(url.lastIndexOf("=") - 2));
+            }
+            
+        @Test
+        public void test1()
+            {
+                Assert.assertEquals("an", "a");
+            }
+    }
