fd5d36d2095abc239b4e33374e2f66bfc50130aa
Alok Sharma <Alok Sharma@QA_Alok-PC-83>
2019-04-26 12:06:15 +0530

    latest update

--- 2.3/src/test/Extent.java
+++ 2.3/src/test/Extent.java
@@ -12,77 +12,78 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class Extent
-    {
+{
 
-        // builds a new report using the html template
-        static ExtentHtmlReporter htmlReporter;
-        
-        static ExtentReports      extent;
-        // helps to generate the logs in test report.
-        static ExtentTest         test;
-        
-        @BeforeClass
-        
-        public static void startTest()
-            
-            {// initialize the HtmlReporter
-                htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\ExtentReportResults.html");
-                
-                // initialize ExtentReports and attach the HtmlReporter
-                extent = new ExtentReports();
-                extent.attachReporter(htmlReporter);
-                
-            }
-            
-        @Test
-        public void test123()
-            {
-                test = extent.createTest("test123");
-                System.out.println("testing");
-                
-            }
-            
-        @Test
-        public void test1234()
-            {
-                test = extent.createTest("test1234");
-                System.out.println("testing");
-                
-            }
-            
-        @Test
-        public void call()
-            {
-                test = extent.createTest("call");
-                aLOK a = new aLOK();
-                a.test();
-                a.test1();
-                
-            }
-            
-        @AfterMethod
-        public void getResult(ITestResult result)
-            {
-                if (result.getStatus() == ITestResult.FAILURE)
-                    {
-                        test.log(Status.FAIL, "Test Case Failed is " + result.getName());
-                        test.fail(result.getThrowable());
-                    } else if (result.getStatus() == ITestResult.SUCCESS)
-                    {
-                        test.log(Status.PASS, "Test Case Passed is " + result.getName());
-                    }
-                    
-                // report.endTest(Logger);
-            }
-            
-        @AfterClass
-        
-        public static void endTest()
-            
-            {
-                
-                extent.flush();
-                
-            }
-            
-    }
+	// builds a new report using the html template
+	static ExtentHtmlReporter htmlReporter;
+
+	static ExtentReports extent;
+	// helps to generate the logs in test report.
+	static ExtentTest test;
+
+	@BeforeClass
+
+	public static void startTest()
+
+	{// initialize the HtmlReporter
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "\\ExtentReportResults.html");
+
+		// initialize ExtentReports and attach the HtmlReporter
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+
+	}
+
+	@Test
+	public void test1()
+	{
+		test = extent.createTest("test1");
+		System.out.println("test1 passed");
+
+	}
+
+	@Test
+	public void test2()
+	{
+		test = extent.createTest("test2");
+		System.out.println("test2 passed");
+
+	}
+
+	@Test
+	public void call()
+	{
+		test = extent.createTest("call");
+		aLOK a = new aLOK();
+		a.test3();
+		a.test4();
+
+	}
+
+	@AfterMethod
+	public void getResult(ITestResult result)
+	{
+		if (result.getStatus() == ITestResult.FAILURE)
+		{
+			test.log(Status.FAIL, "Test Case Failed is " + result.getName());
+			test.fail(result.getThrowable());
+		}
+		else if (result.getStatus() == ITestResult.SUCCESS)
+		{
+			test.log(Status.PASS, "Test Case Passed is " + result.getName());
+		}
+
+		// report.endTest(Logger);
+	}
+
+	@AfterClass
+
+	public static void endTest()
+
+	{
+
+		extent.flush();
+
+	}
+
+}
