05bb6c7b284093fd6d9f37b9c8810ddbd8f1ee19
Alok <Alok@LAPTOP-1JHII323>
2019-05-02 23:24:03 +0530

    ji

--- 2.3/src/test/aLOK.java
+++ 2.3/src/test/aLOK.java
@@ -4,18 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class aLOK
-{
-
-	@Test
-	public void test3()
-	{
-		
-		System.out.println("test3 passes successfully");
-	}
-
-	@Test
-	public void test4()
-	{
-		Assert.assertEquals("an", "a");
-	}
-}
+    {
+        
+        @Test
+        public void test3()
+            {
+                
+                System.out.println("test3 passes successfully");
+            }
+            
+        @Test
+        public void test4()
+            {
+                Assert.assertEquals("abn", "a");
+            }
+    }
